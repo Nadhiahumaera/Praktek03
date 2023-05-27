93215adacc9a3a07058d3ce37dfe507b86bc8157
nadhiahumaera <nadhiahumaera0@gmail.com>
2023-05-27 21:59:57 +0700

    buat Getter and Setter

--- src/praktek03/Sepeda.java
+++ src/praktek03/Sepeda.java
@@ -3,11 +3,38 @@ package praktek03;
 public class Sepeda {
     private String merk;
     private String warna;
-    privatedouble harga;
+    private double harga;
     
     void cetakInfo(){
         System.out.println("Merk \t: "+merk+"\n"+ 
         "Warna \t: "+warna+"\n"+
         "Harga \t:"+harga);
     }
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+    public String getWarna() {
+        return warna;
+    }
+
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+    
+    
+    
 }
